cc36b9e74b9322d464b1a2f670287ffb46110078
ilovedangomilk <123913697+ilovedangomilk@users.noreply.github.com>
2024-06-14 01:44:09 +0800

    Minor changes to IP addresses

--- be/app.py
+++ be/app.py
@@ -168,5 +168,11 @@ def upload_receipt(user_id):
     return jsonify({'extracted_text': extracted_items, 'user_id': user_id})
 
 
+# DONT DELETE THIS, FOR ZHIYI TO USE
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0/0', debug=True)
+
+
+# DONT DELETE THIS, FOR EDWARD TO USE
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0/0', debug=True)
