5afe235894dc123cb0478e0b7e1a293dc8a56cf0
Benhzy <benhzy98@gmail.com>
2024-06-15 18:39:33 +0800

    Update IpAdr.js

--- frontend_react/src/components/IpAdr.js
+++ frontend_react/src/components/IpAdr.js
@@ -1,7 +1,2 @@
-// // update to your own IP address
-// export const apiUrl = 'http://192.168.1.6:5000';
-
-
-// Ed's
 // update to your own IP address
 export const apiUrl = 'code-green-m7oqq06r9-zhiyi-haos-projects.vercel.app';
