ae35a1c8da60c1f425ec9af42bef796e76d47ecc
Benhzy <107015176+Benhzy@users.noreply.github.com>
2024-06-16 01:20:19 +0800

    Update IpAdr.js

--- frontend_react/src/components/IpAdr.js
+++ frontend_react/src/components/IpAdr.js
@@ -1,2 +1,2 @@
 // update to your own IP address
-export const apiUrl = 'https://code-green-aqs2scm82-zhiyi-haos-projects.vercel.app';
+export const apiUrl = 'https://code-green-backend.vercel.app/';
